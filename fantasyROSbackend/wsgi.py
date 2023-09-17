a84b1ca2949f7790c7b14e8f4bd7eb9624d63420
Jacob Anderson <jacob@thepaperlessagent.com>
2023-09-17 06:20:04 -0500

    changed the uswgi file

--- fantasyROSbackend/wsgi.py
+++ fantasyROSbackend/wsgi.py
@@ -7,10 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# add the hellodjango project path into the sys.path
+sys.path.append("/opt/FantasyROS/fantasyROSbackend")
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append("/opt/venv/Lib/site-packages")
+
+# poiting to the project settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantasyROSbackend.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasyROSbackend.settings')
-
 application = get_wsgi_application()
+
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fantasyROSbackend.settings')
+
+# application = get_wsgi_application()
